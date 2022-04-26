5e5f0b3b205b3a430da0db5e42edfff4062ecf49
DinOfficial <shdinofficial19@gmail.com>
2022-04-27 01:17:49 +0600

    inital commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('layouts.partials.head')
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+        @include('layouts.partials.navbar')
+        <!-- Main Sidebar Container -->
+        @include('layouts.partials.sidebar')
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0">Starter Page</h1>
+                        </div><!-- /.col -->
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item active">Starter Page</li>
+                            </ol>
+                        </div><!-- /.col -->
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            @yield('content')
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+
+        <!-- Main Footer -->
+        @include('layouts.partials.footer')
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- REQUIRED SCRIPTS -->
+    @include('layouts.partials.footerScript')
+</body>
+
+</html>
